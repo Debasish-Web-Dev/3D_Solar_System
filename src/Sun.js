25db737bbec1efc1e50bc9058d65bf5bcc911c65
Debasish Das <ddas70896@gmail.com>
2025-07-03 12:54:39 +0530

    first commit

---
+++ src/Sun.js
@@ -0,0 +1,23 @@
+import * as THREE from "three";
+
+function getSun() {
+  const loader = new THREE.TextureLoader();
+  const img = `sunmap.jpg`
+  const path = `./Img/${img}`;
+
+  const sunMat = new THREE.MeshStandardMaterial({
+    map: loader.load(path),
+    bumpMap: loader.load(path),
+    bumpScale: 0.1,
+    emissive: 0xFF7000,
+  });
+  const geo = new THREE.IcosahedronGeometry(1, 16);
+  const sun = new THREE.Mesh(geo, sunMat);
+
+  const sunLight = new THREE.PointLight(0xFFF2CC, 50);
+  sun.add(sunLight);
+
+  return sun;
+}
+
+export default getSun;
